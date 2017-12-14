3d97eda5bcfcde6b19ce42657500f1d6537642fb
diwap <dipipandey1@gmail.com>
2017-12-14 23:40:14 +0545

    Application initialized for balance score card

--- bsc/__manifest__.py
+++ bsc/__manifest__.py
@@ -3,33 +3,27 @@
     'name': "bsc",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Balance Score Card is an indicator""",
 
     'description': """
-        Long description of module's purpose
+        Balance Score Card`
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Diwakar Pandey",
+    'website': "http://www.linkedin.in/diwap",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'management',
+    'version': '11.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
