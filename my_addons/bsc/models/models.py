b5d5438beb61494b8388bc81838437efa4d06de6
diwap <dipipandey1@gmail.com>
2018-04-28 23:25:30 +0545

    validation, bug fix and get_default_value
    validation added in at least one field in a class to make sure empty field cannot be added  
    minor relational bug fixed  
    when child view is opened from parent view, it automatically get and set id of child relation field

--- my_addons/bsc/models/models.py
+++ my_addons/bsc/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class Bsc(models.Model):
 	_name = 'bsc.bsc'
@@ -13,7 +14,8 @@ class Bsc(models.Model):
 		('learning','Learning'),
 	]
 
-	name = fields.Many2one('res.partner',"Name")
+	name = fields.Char("Name", required=True)
+	company_id = fields.Many2one('res.partner',"Company Name")
 	category = fields.Selection(CATEGORY_SELECTION, string="Category", default='financial')
 	objective_bsc_ids = fields.One2many('bsc.objective','objective_bsc_ids')
 
@@ -24,12 +26,12 @@ class Objective(models.Model):
 
 	# @api.model
     # def _default_collaborator(self):
-    #     """ When active_model is res.partner, the current partners should be attendees """
+    #     """ When active_model is res.users, the current partners should be attendees """
     #     partners = self.env.user.partner_id
     #     active_id = self._context.get('active_id')
-    #     if self._context.get('active_model') == 'res.partner' and active_id:
+    #     if self._context.get('active_model') == 'res.users' and active_id:
     #         if active_id not in partners.ids:
-    #             partners |= self.env['res.partner'].browse(active_id)
+    #             partners |= self.env['res.users'].browse(active_id)
     #     return partners
 
 	# filter_user = """
@@ -38,12 +40,14 @@ class Objective(models.Model):
     #     """
 
 	title = fields.Char("Title", required=True)
-	owner = fields.Many2one('res.partner', "Owner")
+	owner = fields.Many2one('res.users',
+		string= "Owner",
+		default=lambda self: self.env.uid)
 	objective_bsc_ids = fields.Many2one('bsc.bsc',"BSC")
 	# recommendation_objective_ids = fields.One2many('bsc.recommendation','recommendation_objective_ids')
 	analysis = fields.Text("Analysis")
 	description = fields.Text("Description")
-	collaborator_ids = fields.Many2many('res.partner','bsc_objective_res_partner_rel', string="Collaborators")
+	collaborator_ids = fields.Many2many('res.users', string="Collaborators")
 	measure_objective_ids = fields.One2many('bsc.measure','measure_objective_ids')
 
 class Measure(models.Model):
@@ -51,24 +55,29 @@ class Measure(models.Model):
 	_inherit = 'mail.thread'
 	_rec_name = 'title'
 
-	title = fields.Char("Title")
+	title = fields.Char("Title", required=True)
 	analysis = fields.Text("Analysis")
 	# recommendation_measure_ids = fields.One2many('bsc.recommendation','recommendation_measure_ids')
 	measure_data_measure_ids = fields.One2many('bsc.measuredata','measure_data_measure_ids')
-	owner = fields.Many2one('res.partner',"Owner")
-	collaborator_ids = fields.Many2many('res.partner','bsc_measure_res_partner_rel', string="Collaborators")
+	initiative_measure_ids = fields.One2many('bsc.initiative','measure_id')
+	owner = fields.Many2one('res.users',
+		string= "Owner",
+		default=lambda self: self.env.uid)
+	collaborator_ids = fields.Many2many('res.users', string="Collaborators")
 	description = fields.Text("Description")
-	measure_objective_ids = fields.Many2one('bsc.objective',"Measures")
+	measure_objective_ids = fields.Many2one('bsc.objective',"Objective")
 
 class Initiative(models.Model):
 	_name = 'bsc.initiative'
 	_inherit = 'mail.thread'
 	_rec_name = 'title'
 
-	title = fields.Char("Title")
+	title = fields.Char("Title", required=True)
 	measure_id = fields.Many2one('bsc.measure',"Measure")
-	owner = fields.Many2one('res.partner',"Owner")
-	collaborator_ids = fields.Many2many('res.partner','bsc_initiative_res_partner_rel', string="Collaborators")
+	owner = fields.Many2one('res.users',
+		string= "Owner",
+		default=lambda self: self.env.uid)
+	collaborator_ids = fields.Many2many('res.users','bsc_initiative_res_users_rel', string="Collaborators")
 	budget = fields.Float("Budget")
 	description = fields.Text("Description")
 	percent_complete = fields.Float("Percent Complete")
@@ -86,32 +95,38 @@ class Initiative(models.Model):
 # 	_rec_name = 'text'
 
 # 	text = fields.Text("Text")
-# 	posted_by = fields.Many2one('res.partner',"Posted By")
+# 	posted_by = fields.Many2one('res.users',"Posted By")
 # 	recommendation_objective_ids = fields.Many2one('bsc.objective',"Recommendations")
 # 	recommendation_measure_ids = fields.Many2one('bsc.measure',"Recommendation")
 # 	recommendation_initiative_ids = fields.Many2one('bsc.initiative',"Recommendation")
 
 class MeasureData(models.Model):
 	_name = 'bsc.measuredata'
+	_rec_name = 'measure_data_measure_ids'
 
-	measure_data_measure_ids = fields.Many2one('bsc.measure',"Measure Data")
+	measure_data_measure_ids = fields.Many2one('bsc.measure',"Measure", required=True)
 	period = fields.Date("Period")
 	actual = fields.Float("Actual")
 	target = fields.Float("Target")
-	variance = fields.Float("Variance")
-	percent_of_target = fields.Float("Percent of Target")
+	variance = fields.Float("Variance", compute='_get_variance')
+
+	def _get_variance(self):
+		for val in self:
+			val.variance =  (val.actual - val.target)/abs(val.target)*100
 
 class Milestone(models.Model):
 	_name = 'bsc.milestone'
 	_rec_name = 'title'
 
-	title = fields.Char("Title")
-	milestone_initiative_ids = fields.Many2one('bsc.initiative',"Milestone")
-	owner = fields.Many2one('res.partner',"Owner")
+	title = fields.Char("Title", required=True)
+	milestone_initiative_ids = fields.Many2one('bsc.initiative',"Initiative")
+	owner = fields.Many2one('res.users',
+		string= "Owner",
+		default=lambda self: self.env.uid)
 	percent_complete = fields.Float("Percent Complete")
 	analysis = fields.Text("Analysis")
 	recommendation = fields.Text("Recommendation")
-	collaborator_ids = fields.Many2many('res.partner','bsc_milestone_res_partner_rel', string="Collaborators")
+	collaborator_ids = fields.Many2many('res.users','bsc_milestone_res_users_rel', string="Collaborators")
 	description = fields.Text("Description")
 	start_date = fields.Date("Start Date")
 	end_date = fields.Date("End Date")
@@ -126,11 +141,13 @@ class Action(models.Model):
 	_name = 'bsc.action'
 	_rec_name = 'name'
 
-	name = fields.Char("Name")
+	name = fields.Char("Name", required=True)
 	action_initiative_ids = fields.Many2one('bsc.initiative',"Initiative Action")
 	action_milestone_ids = fields.Many2one('bsc.milestone',"Milestone Action")
-	owner = fields.Many2one('res.partner',"Owner")
-	collaborator_ids = fields.Many2many('res.partner','bsc_action_res_partner_rel', string="Collaborators")
+	owner = fields.Many2one('res.users',
+		string= "Owner",
+		default=lambda self: self.env.uid)
+	collaborator_ids = fields.Many2many('res.users','bsc_action_res_users_rel', string="Collaborators")
 	description = fields.Text("Description")
 	comment = fields.Text("Comment")
 	start_date = fields.Date("Start Date")
