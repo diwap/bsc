8e2e40d21778cb59c60fbed91cd6830faba36ef7
Diwap <dipipandey1@gmail.com>
2018-05-04 15:13:39 +0545

    id added for redirecting to respective record

--- my_addons/bsc/models/dashboard.py
+++ my_addons/bsc/models/dashboard.py
@@ -7,49 +7,26 @@ class BscDashboard(models.Model):
     def get_bsc_data(self):
         bsc = self.env['bsc.bsc'].search([('active','=',True)])
         data = []
-        # objectives = []
-        # measure = []
-        # initiative = []
-        # for rec in bsc:
-        #     for re in rec.objective_bsc_ids:
-        #         objectives.append({
-        #             'name': re.title,
-        #             'owner': re.owner.name
-        #         })
-        # for rec in bsc:
-        #     for m in bsc.measure_bsc_ids:
-        #         measure.append({
-        #             'title': m.title,
-        #             'owner': m.owner.name
-        #         })
-        # for rec in bsc:
-        #     for i in bsc.initiative_bsc_ids:
-        #         initiative.append({
-        #             'title': i.title,
-        #             'owner': i.owner.name
-        #         })
         for d in bsc:
             data.append({
+                'id': d.id,
                 'name': d.name,
                 'category': d.category,
                 'objectives': [{
+                    'id': obj.id,
                     'name': obj.title,
                     'owner': obj.owner.name
                 }for obj in d.objective_bsc_ids],
                 'measure': [{
+                    'id': m.id,
                     'title': m.title,
                     'owner': m.owner.name
                 } for m in d.measure_bsc_ids],
                 'initiative': [{
+                    'id': i.id,
                     'title': i.title,
-                    'owner': i.owner.name
+                    'owner': i.owner.name,
+                    'percent_complete': i.percent_complete
                 } for i in d.initiative_bsc_ids]
             })
-        # ser_rec = self.env['bsc.bsc'].search_read([])
-        # print (ser_rec)
-        # print ("\n\n\n\n\n\n\n")
-        # print (ser_rec)
-        # print ("\n\n\n\n\n\n\n")
-        # print (", ".join( repr(e) for e in data ))
-        # return (", ".join( repr(e) for e in data ))
         return data
